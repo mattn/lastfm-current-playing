eef7903bd19636929d5883282f12af410404a9fa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-03-08 12:25:17 +0900

    fix

--- main.go
+++ main.go
@@ -1,36 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/ndyakov/go-lastfm"
+    "github.com/ndyakov/go-lastfm"
 )
 
 func main() {
-	apiKey := os.Getenv("LASTFM_API_KEY")
-	apiSecret := os.Getenv("LASTFM_API_SECRET")
-	user := os.Getenv("LASTFM_USER")
+    apiKey := os.Getenv("LASTFM_API_KEY")
+    apiSecret := os.Getenv("LASTFM_API_SECRET")
+    user := os.Getenv("LASTFM_USER")
 
-	api := lastfm.New(apiKey, apiSecret)
+    api := lastfm.New(apiKey, apiSecret)
+    response, err := api.User.GetRecentTracks(user, 0, 1, 0, 0)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	var lastStatus string
-	for {
-		response, err := api.User.GetRecentTracks(user, 0, 1, 0, 0)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for _, track := range response.RecentTracks {
-			if track.NowPlaying == "" {
-				continue
-			}
-			status := fmt.Sprintf("%s - %s\n", track.Artist.Name, track.Name)
-			if status == lastStatus {
-				continue
-			}
-			lastStatus = status
-		}
-	}
+    for _, track := range response.RecentTracks {
+        if track.NowPlaying == "" {
+            continue
+        }
+        fmt.Printf("%s - %s\n", track.Artist.Name, track.Name)
+        break
+    }
 }
